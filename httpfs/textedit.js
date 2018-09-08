cb2e3b401a252ff013f8ff2204174e321b782b43
Kyle <user@swift3.hollandnet.local>
2018-09-08 09:16:25 -0600

    basic demo text editor (doesn't save yet)

---
+++ httpfs/textedit.js
@@ -0,0 +1,148 @@
+function(args, stdin, stdout, stderr) {
+  if(args.length != 2) { syscall_pipe_write(stderr, "Bad args\n"); return; }
+  var file = shell_process_relative(ENV_CWD, args[1]);
+  if(!fs_exists(file)) { syscall_pipe_write(stderr, file + ": No such file or directory\n"); return; }
+  if(fs_isdir(file)) { syscall_pipe_write(stderr, file + ": Is a directory\n"); return; }
+  var fd = fs_open(file);
+  if(fd == null) { syscall_pipe_write(stderr, "Error opening file\n"); return; }
+  var buf = syscall_fd_read(fd, syscall_fd_info(fd).size);
+  fs_close(fd);
+  
+  var size = {width: 576, height: 768};
+  var gfx_id = libwm_create(size.width, size.height);
+  gfx_fillrect(gfx_id, 0, 0, size.width, size.height, "#FFFFFF");
+  
+  var FONT_SIZE = 12;
+  var LINE_SIZE = 16;
+  //var buf = "";
+  var offset = {x: 0, y: 0};
+  var cursor = {x: 0, y: LINE_SIZE, line: 0, col: 0, pos: 0, visible: true, flashState: true};
+  
+  function render() {
+    gfx_fillrect(gfx_id, 0, 0, size.width, size.height, "#FFFFFF");
+    
+    var lines = buf.split("\n");
+    var maxLines = Math.floor(gfx_get_size(gfx_id).height / LINE_SIZE);
+    
+    for(var i = 0; i < Math.min(lines.length, maxLines); i++) {
+      gfx_text(gfx_id, FONT_SIZE.toString() + "px monospace", "#000000", lines[i + offset.y], 0, LINE_SIZE * (i + 1));
+    }
+    
+    if(cursor.visible && cursor.flashState) {
+      //some cosmetic adjustments on cursor positioning
+      gfx_line(gfx_id, cursor.x, cursor.y + 1, cursor.x, cursor.y + 1 - FONT_SIZE, "#000000", 0.5);
+    }
+  }
+  
+  //recalc cursor properties based on cursor.pos
+  function calcCursor() {
+    var sub = buf.substring(0, cursor.pos);
+    cursor.line = (sub.match(/\n/g) || []).length;
+    var lines = buf.split("\n");
+    cursor.col = sub.length - sub.lastIndexOf("\n") - 1;
+    
+    cursor.x = gfx_measuretext(gfx_id, FONT_SIZE.toString() + "px monospace", lines[cursor.line].substring(0, cursor.col)).width;
+    cursor.y = ((cursor.line + 1) - offset.y) * LINE_SIZE; //add one to line b/c canvas text uses bottom as position
+    
+    cursor.flashState = true;
+    
+    //TODO: scroll if cursor is off screen
+  }
+  
+  //move the cursor on click
+  gfx_bind_click(gfx_id, function(x, y) {
+    var line = Math.floor(y / LINE_SIZE) + offset.y; /* round down to nearest line and apply offset */
+    
+    var char = 0;
+    var charX = 0;
+    var lines = buf.split("\n");
+    if(line >= lines.length) { return; }
+    var lineData = lines[line];
+    for(var i = 0; i <= lineData.length; i++) {
+      var wThis = gfx_measuretext(gfx_id, FONT_SIZE.toString() + "px monospace", lineData.substring(0, i)).width;
+      var wNext = null;
+      if(i + 1 <= lineData.length) { wNext = gfx_measuretext(gfx_id, FONT_SIZE.toString() + "px monospace", lineData.substring(0, i + 1)).width; }
+      
+      if(wNext == null || x < ((wThis + wNext) / 2)) {
+        char = i;
+        charX = Math.floor(wThis);
+        break;
+      }
+    }
+    
+    cursor.x = charX;
+    cursor.y = ((line + 1) - offset.y) * LINE_SIZE; //add one to line b/c canvas text uses bottom as position
+    cursor.line = line;
+    cursor.col = char;
+    cursor.pos = 0;
+    for(var i = 0; i < line; i++) {
+      cursor.pos += lines[i].length + 1;
+    }
+    cursor.pos += char;
+    
+    cursor.flashState = true;
+    
+    render();
+  });
+  
+  //keyboard input
+  gfx_bind_keypress(gfx_id, function(e) {
+    //prevent Firefox quick find - https://greasyfork.org/en/scripts/13484-prevent-slash-opening-quickfind
+    if(e.charCode == 47 || e.charCode == 39) {
+      e.preventDefault();
+    }
+    //capture tab
+    if(e.keyCode == 9) {
+      e.preventDefault();
+    }
+    
+    var char = null;
+    
+    if(e.keyCode == 13) { //enter
+      char = "\n";
+    } else if(e.keyCode == 8) { //backspace
+      char = "\b";
+    } else if(e.keyCode == 9) { //tab
+      char = "\t";
+    } else if(e.charCode != 0) {
+      char = String.fromCharCode(e.charCode); //FIXME - docs say e.char should be used https://developer.mozilla.org/en-US/docs/Web/Events/keypress
+    }
+    
+    //TODO: delete, home, end, etc.
+    //TODO: auto-indent and \t -> spaces
+    if(char == "\b") {
+      if(cursor.pos > 0) {
+        buf = buf.substring(0, cursor.pos - 1) + buf.substring(cursor.pos);
+        cursor.pos--;
+        calcCursor();
+      }
+    } else if(char != null) {
+      buf = buf.substring(0, cursor.pos) + char + buf.substring(cursor.pos);
+      cursor.pos++;
+      calcCursor();
+    } else {
+      //no char - do u/d/l/r keys
+      if(e.keyCode == 38) { //up
+        
+      } else if(e.keyCode == 40) { //down
+        
+      } else if(e.keyCode == 37) { //left
+        if(cursor.pos > 0) { cursor.pos--; }
+        calcCursor();
+      } else if(e.keyCode == 39) { //right
+        if(cursor.pos < buf.length) { cursor.pos++; }
+        calcCursor();
+      }
+    }
+    render();
+  });
+  
+  //cursor flashing
+  gfx_register_interval(gfx_id, setInterval(function() {
+    cursor.visible = libwm_get(gfx_id).focus;
+    cursor.flashState = !cursor.flashState;
+    render(); //TODO: only partial rerender?
+  }, 500));
+  
+  render();
+}
